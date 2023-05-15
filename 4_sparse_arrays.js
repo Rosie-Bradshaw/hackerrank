f8dc7b4ca821e3c3120ff64803a9ef1f7a3c95db
Rosie <rosie.bradshaw3@gmail.com>
2023-05-15 15:04:08 +0200

    sparse arrays complete

--- 4_sparse_arrays.js
+++ 4_sparse_arrays.js
@@ -5,3 +5,22 @@
 // queries = ['ab', 'abc', 'bc']
 
 // There are 2 instances of 'ab', 1 of 'abc' and 0 of 'bc'. For each query, add an element to the return array, results = [2, 1, 0].
+
+
+function matchingStrings (strings, queries) {
+    let matches = []
+
+    for (let i = 0; i < queries.length; i++) {
+        matches[i] = 0;
+        for (let j = 0; j < strings.length; j++) {
+            if (strings[j] == queries[i]) {
+                matches[i]++
+            }
+        }
+    } 
+    return matches
+}
+
+strings = ['ab', 'ab', 'abc']
+queries = ['ab', 'abc', 'bc']
+console.log(matchingStrings(strings, queries))
